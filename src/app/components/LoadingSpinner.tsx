655d3c6c1baa08c8d401dc46d9c2043dc6fdc447
Rotem Kincaid <rotema23@gmail.com>
2024-08-06 16:01:28 -0700

    First commit

---
+++ src/app/components/LoadingSpinner.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const LoadingSpinner = () => {
+  return (
+    <div className="flex">
+        <div className="relative"> 
+            <div className="w-12 h-12 rounded-full absolute border-4 border-solid border-gray-200"></div>
+            <div className="w-12 h-12 rounded-full animate-spin absolute border-4 border-solid border-green-500 border-t-transparent shadow-md"></div>
+        </div>
+    </div>
+)
+}
+
+
+
+export default LoadingSpinner
